777bb4dee618a0b0ade4d644768a8790ac7e4e35
Aniket762 <aniketindian8@gmail.com>
2021-04-26 22:35:19 +0530

    Initialized

---
+++ client/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+import { SnackbarProvider } from 'notistack';
+
+import history from './Utilities/history';
+import PrivateRoute from './Utilities/private-route';
+import Home from './Home/Home';
+import Chat from './Chat/Chat';
+import Landing from './Landing/Landing';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#58a5f0',
+            main: '#ff0000',
+            dark: '#004c8c',
+        },
+        secondary: {
+            light: '#ffd95a',
+            main: '#f9a825',
+            dark: '#c17900',
+            contrastText: '#212121',
+        },
+        background: {
+            default: '#fff',
+        },
+    },
+    typography: {
+        useNextVariants: true,
+    },
+});
+
+function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
+                <Router history={history}>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/landing" exact component={Landing} />
+                    <PrivateRoute path="/chat" component={Chat} />
+                </Router>
+            </SnackbarProvider>
+        </ThemeProvider>
+    );
+}
+
+export default App;
---
+++ client/src/Landing/Landing.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './landing.css';
+
+function Landing() {
+    return (
+        <div class="container-fluid homepage-bgimage"></div>
+    )
+}
+
+export default Landing
